fa72b2f2970dc5643314016f4715b83231c43f0e
Jonne Haapalainen <jonne.haapalainen@eniram.fi>
2020-03-10 15:28:06 +0200

    Updates

---
+++ precache-manifest.4a68355c0561985b6e097626efe6d2bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942e6d0480dc1d556f4c899f41ca0375",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "746d3563ad17376b3aac",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34f5470a048a810751bf",
+    "url": "/test/static/js/2.8364879f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test/static/js/2.8364879f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746d3563ad17376b3aac",
+    "url": "/test/static/js/main.29b69dae.chunk.js"
+  },
+  {
+    "revision": "6bbc3f51202c28fa0a7b",
+    "url": "/test/static/js/runtime-main.5b4329d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
